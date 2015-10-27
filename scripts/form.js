b98afef58dae84a2fef2a5bd122a689cda59f94e
Tetsuya Matsushita <s.c.w-victory.m@r3.dion.ne.jp>
2015-10-27 21:01:17 +0900

    P.135 3-7-2まで(3-7-2は読んだだけで演習は省略)。

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,9 @@
 angular.module("myApp", ["ngMessages"])
     .controller("MyController", ["$scope", function($scope) {
-        
+        $scope.errors = [
+            { type: "required", message: "入力は必須です。" },
+            { type: "maxlength", message: "文字数が多すぎます。"},
+            { type: "minlength", message: "文字数が少なすぎます。"},
+            { type: "email", message: "正しいメールアドレスを入力してください。"}
+        ];
     }])
